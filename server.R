5650c2c516887cd838f9179d1ac532220c466b9e
TomNash <nashtf@g.cofc.edu>
2017-02-27 10:02:19 -0500

    New version with Leaflet

--- server.R
+++ server.R
@@ -1,194 +1,292 @@
-options(warn=-1)
+# options(warn=-1)
 
-list.of.packages <- c("shiny", "shinyBS", "openxlsx", "raster", "dismo","rJava", "maps", 
-"utils", "DT")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+# leaflet(options = leafletOptions(worldCopyJump = T)) %>%
+#   addTiles(
+#     urlTemplate = "//api.mapbox.com/styles/v1/nashtf/ciziymwzy002i2smqmvqvygv7/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFzaHRmIiwiYSI6ImNpeml5bTc4NzAybTIzM21pN2NoeXJseXMifQ.NwM6pg94SamDh9vVRxpmgA.png",
+#     attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+#   ) %>% addRasterImage(maxent.map) %>% setView(lng = -93.85, lat = 37.45, zoom = 2)
+# %>% addCircleMarkers(lng=occ$Long[1:10], lat=occ$Lat[1:10], weight=1) %>% addPopups(lng=occ$Long[1:10], lat = occ$Lat[1:10], "hi")
 
-require(shiny)
-require(shinyBS)
-require(raster)
-require(dismo)
-require(openxlsx)
-require(rJava)
-require(maps)
-require(utils)
-
-jar <- paste(system.file(package="dismo"), "/java/maxent.jar", sep='')
-
-if (!file.exists(jar)) {
-  stop("Download the required 'maxent.jar' file and palce in the appropriate directory")
-}
+# leaflet() %>%
+#   addTiles(
+#     urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+#     attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+#   ) %>%
+#   setView(lng = -93.85, lat = 37.45, zoom = 4) %>% addPopups(lng=occ$Long[1:10], lat = occ$Lat[1:10], "hi")
 
 
-unzip("data.zip", overwrite = T)
+# list.of.packages <- c("shiny", "shinyBS", "openxlsx", "raster", "dismo","rJava", "maps", 
+# "utils", "DT")
+# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+# if(length(new.packages)) install.packages(new.packages)
+print("First")
 
-# Read in file and fix typos
-raw.data <- read.xlsx("data/Raw_GH_Vaccine_Map.xlsx")
-raw.data[which(raw.data$Impact.Scale == "Isolated "),11] <- "Isolated"
-raw.data[which(raw.data$Impact.Scale == "Seconday "),11] <- "Secondary"
-raw.data[which(raw.data$Impact.Scale == "Seconday"),11] <- "Secondary"
-raw.data[which(raw.data$Outbreak == "Measles\n"),2] <- "Measles"
-raw.data[which(raw.data$Outbreak == "Diphtheria"),2] <- "Diptheria"
-raw.data[which(raw.data$Outbreak == "Typhoid Fever\n"),2] <- "Typhoid Fever"
-raw.data[grepl("Polio*",raw.data$Outbreak),2] <- "Polio"
-raw.data[which(raw.data$Outbreak == "Mumps\n"),2] <- "Mumps"
-raw.data[which(raw.data$Outbreak == "Whooping Cough\n"),2] <- "Whooping Cough"
-raw.data[grepl("Measles*",raw.data$Outbreak),2] <- "Measles"
-raw.data[which(raw.data$Outbreak == "Annoucement"),2] <- "Announcement"
-raw.data[which(raw.data$Outbreak == "Announcement "),2] <- "Announcement"
-raw.data[which(raw.data$Year=="2101"),9] <- "2011"
-raw.data[which(raw.data$Year=="214"),9] <- "2014"
-raw.data[778, "Long"] <- "-3.16017"
-raw.data[1051, "Lat"] <- "44.7300"
+library(shiny)
+library(shinyBS)
+library(raster)
+library(dismo)
+library(openxlsx)
+library(rJava)
+library(maps)
+library(utils)
+library(tools)
+library(ggmap)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+library(shinyjs)
+library(randomForest)
 
-# Remove bad data
-raw.data <- raw.data[-385, ]
-raw.data <- raw.data[-which(raw.data$Impact.Scale == "Announcement"), ]
-raw.data <- raw.data[-which(is.na(raw.data$Impact.Scale)), ]
-raw.data <- raw.data[-which(raw.data$Impact.Scale == "."), ]
+# jar <- paste(system.file(package="dismo"), "/java/maxent.jar", sep='')
+# 
+# if (!file.exists(jar)) {
+#   stop("Download the required 'maxent.jar' file and palce in the appropriate directory")
+# }
 
-# Convert Latitude, Longitude, and years to numerics
-raw.data$Lat <- as.numeric(raw.data$Lat)
-raw.data$Long <- as.numeric(raw.data$Long)
-raw.data$Year <- as.numeric(raw.data$Year)
-raw.data$Fatalities <- as.numeric(raw.data$Fatalities)
-raw.data$Impact.Scale <- as.factor(raw.data$Impact.Scale)
 
-# Subset the rows, columns desired
-data <- subset(raw.data, Outbreak %in% c("Measles", "Mumps", "Polio", "Rubella",
-                                         "Whooping Cough"),
-               select=c(Outbreak, Year, Long, Lat, Fatalities, Impact.Scale))
+# unzip("data.zip", overwrite = T)
+# 
+# # Read in file and fix typos
+# outbreak.data <- read.xlsx("data/Raw_GH_Vaccine_Map.xlsx")
+# outbreak.data[which(outbreak.data$Impact.Scale == "Isolated "),11] <- "Isolated"
+# outbreak.data[which(outbreak.data$Impact.Scale == "Seconday "),11] <- "Secondary"
+# outbreak.data[which(outbreak.data$Impact.Scale == "Seconday"),11] <- "Secondary"
+# outbreak.data[which(outbreak.data$Outbreak == "Measles\n"),2] <- "Measles"
+# outbreak.data[which(outbreak.data$Outbreak == "Diphtheria"),2] <- "Diptheria"
+# outbreak.data[which(outbreak.data$Outbreak == "Typhoid Fever\n"),2] <- "Typhoid Fever"
+# outbreak.data[grepl("Polio*",outbreak.data$Outbreak),2] <- "Polio"
+# outbreak.data[which(outbreak.data$Outbreak == "Mumps\n"),2] <- "Mumps"
+# outbreak.data[which(outbreak.data$Outbreak == "Whooping Cough\n"),2] <- "Whooping Cough"
+# outbreak.data[grepl("Measles*",outbreak.data$Outbreak),2] <- "Measles"
+# outbreak.data[which(outbreak.data$Outbreak == "Annoucement"),2] <- "Announcement"
+# outbreak.data[which(outbreak.data$Outbreak == "Announcement "),2] <- "Announcement"
+# outbreak.data[which(outbreak.data$Year=="2101"),9] <- "2011"
+# outbreak.data[which(outbreak.data$Year=="214"),9] <- "2014"
+# outbreak.data[778, "Long"] <- "-3.16017"
+# outbreak.data[1051, "Lat"] <- "44.7300"
+# 
+# # Remove bad data
+# outbreak.data <- outbreak.data[-385, ]
+# outbreak.data <- outbreak.data[-which(outbreak.data$Impact.Scale == "Announcement"), ]
+# outbreak.data <- outbreak.data[-which(is.na(outbreak.data$Impact.Scale)), ]
+# outbreak.data <- outbreak.data[-which(outbreak.data$Impact.Scale == "."), ]
+# 
+# # Convert Latitude, Longitude, and years to numerics
+# outbreak.data$Lat <- as.numeric(outbreak.data$Lat)
+# outbreak.data$Long <- as.numeric(outbreak.data$Long)
+# outbreak.data$Year <- as.numeric(outbreak.data$Year)
+# outbreak.data$Fatalities <- as.numeric(outbreak.data$Fatalities)
+# outbreak.data$Impact.Scale <- as.factor(outbreak.data$Impact.Scale)
+
+# # Subset the rows, columns desired
+# data <- subset(outbreak.data, Outbreak %in% c("Measles", "Mumps", "Polio", "Rubella",
+#                                          "Whooping Cough"),
+#                select=c(Outbreak, Year, Long, Lat, Fatalities, Impact.Scale))
 
 # Load in the bioClim raster data
+
 load("data/bioclim_10m.Rdata")
 
 # Load in the population density raster data
 # Has to be modified to fit dimensions of bioClim
 # Padding blank spaces to cover same space
+
 pop.dens <- raster("data/glds00ag.bil")
 projection(pop.dens) <- "+proj=longlat +ellps=WGS84"
 e <- extent(-180,180,-60,90)
 pop.dens <- extend(pop.dens,e)
 pop.dens <- aggregate(pop.dens, fac=4)
+merge.socio.climate <- addLayer(bioStack, pop.dens)
+run.models <- FALSE
 
 shinyServer(
+
   function(input, output, session) {
-   
-    # Reactive plot that visualizes cases before running model
-    output$visualize <- renderPlot({
 
-      # Get range of years
-      year.range <- input$years[1]:input$years[2]
-      
-      # Narrow down to desired outbreak types in desired years based on scale
-      filtered.cases <- which(data$Outbreak %in% input$categories & 
-                                data$Year %in% year.range &
-                                data$Impact.Scale %in% input$impact.scale)
-
-      # Filter on fatality selection
-      if (as.numeric(input$fatalities) > 0) {
-        filtered.cases <- filtered.cases[which(data[filtered.cases, "Fatalities"] > 0)]
-      } else if (as.numeric(input$fatalities) == 0) {
-        filtered.cases <- filtered.cases[which(data[filtered.cases, "Fatalities"] == 0)]
-      } else {
-        filtered.cases <- filtered.cases
-      }
-      
-      # Plot occurences reactive to user selection
-      occ <- data[filtered.cases,3:4]    
-      map('world',interior=FALSE,col='gray', fill=TRUE, lty=0, ylim=c(-60, 90))
-      points(occ, pch=20, cex=0.4)
-      title(main=paste0("Number of outbreaks selected: ", nrow(occ)))
-      mtext("N.B. At least 40 outbreaks are needed for modeling")
+    # Create the map
+    output$map <- renderLeaflet({
+          leaflet(options = leafletOptions(worldCopyJump = T)) %>%
+              addTiles(
+                #urlTemplate = "//api.mapbox.com/styles/v1/nashtf/ciziymwzy002i2smqmvqvygv7/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFzaHRmIiwiYSI6ImNpeml5bTc4NzAybTIzM21pN2NoeXJseXMifQ.NwM6pg94SamDh9vVRxpmgA.png",
+                urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+                attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+              ) %>% setView(lng = -13.85, lat = 37.45, zoom = 2)
     })
-    observeEvent(input$submitButton, {
-      year.range <- input$years[1]:input$years[2]
+    
+    outbreaksInBounds <- reactive({
+      if (is.null(input$map_bounds))
+        return(zipdata[FALSE,])
+      bounds <- input$map_bounds
+      latRng <- range(bounds$north, bounds$south)
+      lngRng <- range(bounds$east, bounds$west)
+      
+      subset(outbreak.data,
+             Lat >= latRng[1] & Lat <= latRng[2] &
+               Long >= lngRng[1] & Long <= lngRng[2])
+    })
+    
+    output$histSize <- renderPlot({
+      # If no zipcodes are in view, don't plot
+      if (nrow(outbreaksInBounds()) == 0)
+        return(NULL)
+      
+      hist(outbreaksInBounds()$centile,
+           breaks = centileBreaks,
+           main = "Outbreak Size",
+           xlab = "Frequency",
+           xlim = range(allzips$centile),
+           col = '#00DD00',
+           border = 'white')
+    })
+    
 
-      # Narrow down to desired outbreak types in desired years based on scale
-      filtered.cases <- which(data$Outbreak %in% input$categories & 
-                                data$Year %in% year.range &
-                                data$Impact.Scale %in% input$impact.scale)
+    observeEvent(input$data, {
+      data.file <- input$data
 
-      # Further filtering on fatalities
-      if (as.numeric(input$fatalities) > 0) {
-        filtered.cases <- filtered.cases[which(data[filtered.cases, "Fatalities"] > 0)]
-      } else if (as.numeric(input$fatalities) == 0) {
-        filtered.cases <- filtered.cases[which(data[filtered.cases, "Fatalities"] == 0)]
-      } else {
-        filtered.cases <- filtered.cases
+      if (is.null(data.file))
+        return(NULL)
+
+      if (file_ext(data.file$name) == "txt")
+        outbreak.data <<- read.table(data.file$datapath)
+      else
+        outbreak.data <<- read.csv(data.file$datapath)
+
+      names(outbreak.data)[grepl("long", names(outbreak.data), ignore.case = T)] <- "Long"
+      names(outbreak.data)[grepl("lat", names(outbreak.data), ignore.case = T)] <- "Lat"
+      names(outbreak.data)[grepl("cases", names(outbreak.data), ignore.case = T)] <- "Cases"
+
+      radius <- log(outbreak.data$Cases) + 5
+      
+      print(names(outbreak.data))
+
+      leafletProxy("map", data = outbreak.data) %>%
+        clearShapes() %>%
+        addCircleMarkers(~Long, ~Lat, radius=radius,
+                   stroke=FALSE, fillOpacity=0.4, fillColor="blue") #%>% addLegend("bottomleft", pal=pal, values=colorData, title=colorBy, layerId="colorLegend")
+      shinyjs::hide(id = "viz")
+      
+    })
+    
+    observe({
+      if (is.null(input$goto))
+        return()
+      isolate({
+        map <- leafletProxy("map")
+        map %>% clearPopups()
+        dist <- 10
+        lat <- input$goto$lat
+        lng <- input$goto$lng
+        showOutbreakInfo(lat, lng)
+        map %>% fitBounds(lng + dist, lat + dist, lng - dist, lat - dist)
+      })
+    })
+    
+    output$table <- DT::renderDataTable({
+      #print(outbreak.data[1,1:11]),
+      #DT::datatable(outbreak.data[,1:11])
+      DT::datatable(outbreak.data[,1:11])
+      df <- outbreak.data %>%
+      select(Outbreak,Location,Continent,Lat,Long,Year,Cases,Impact.Scale) %>%
+      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+      action <- DT::dataTableAjax(session, df)
+      print(df[1,])
+      DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+    })
+
+    # Show a popup at the given location
+    showOutbreakInfo <- function(lat, lng) {
+
+      matched.popups <- subset(outbreak.data, Lat == lat & Long == lng)
+
+      if (nrow(matched.popups) > 0) {
+        revgeo.out <- lapply(1:nrow(matched.popups), function(i) {
+          revgeocode(as.numeric(matched.popups[i,c("Long","Lat")]), output = "more", messaging = F)
+        })
+
+        content <- lapply(1:length(revgeo.out), function(i) {
+          as.character(tagList(
+            tags$h4(matched.popups[i,"Outbreak"]),
+            tags$strong(HTML(sprintf("%s, %s",
+                                     revgeo.out[[i]]$administrative_area_level_1,
+                                     revgeo.out[[i]]$country))),
+            tags$br(),sprintf("Year: %d", matched.popups[i,"Year"]),
+            tags$br(),sprintf("Number of cases: %d", matched.popups[i,"Cases"]),
+            tags$br(),sprintf("Scale: %s", matched.popups[i,"Impact.Scale"]),
+            tags$br()
+          ))
+        })
+        leafletProxy("map") %>% addPopups(lng=matched.popups$Long,
+                                          lat=matched.popups$Lat, content)
       }
-      
+      else {
+        return()
+      }
+    }
+
+    # When map is clicked, show a popup with city info
+    observe({
+      leafletProxy("map") %>% clearPopups()
+      event <- input$map_marker_click
+      if (is.null(event))
+        return()
+      isolate({
+        showOutbreakInfo(event$lat, event$lng)
+      })
+    })
+
+    observeEvent(input$runModel, {
+
       # Make sure there are at least 10 cases
-      validate(need(length(filtered.cases) >= 40, ""))
-      
+      validate(need(nrow(outbreak.data) >= 40, ""))
+
       # Progress message
       progress = shiny::Progress$new()
       on.exit(progress$close())
-      progress$set(message = "Running model. Please wait.", value = 0)
-      
-      
-      # Add the climatic and socioeconomic data to one raster brick
-      merge.socio.climate <- addLayer(bioStack, pop.dens)
+      progress$set(message = "Running MaxEnt model. Please wait.", value=0)
 
-      # Get what indices (layers in the raster brick) to be kept
-      chosen.predictors <- as.numeric(c(input$precipitation, 
-                                        input$temperature,
-                                        input$socio))
       # Get occurences that match filters
-      occ <- data[filtered.cases,3:4]
-      
+      occ <- outbreak.data[,c("Long","Lat")]
+
       # Cross-validation, 80-20 split
       fold <- kfold(occ, k = 5)
       occtest <- occ[fold == 1, ]
       occtrain <- occ[fold != 1, ]
-      
-      # If all predictors are to be used
-      if (length(chosen.predictors) == nlayers(merge.socio.climate)) {
-        maxent.model <- maxent(merge.socio.climate, occtrain)
-      } else {
-        # Drop the layers which aren't to be used
-        merge.socio.climate <- dropLayer(merge.socio.climate, 
-                                         i = c(1:nlayers(merge.socio.climate))[-chosen.predictors])
-        maxent.model <- maxent(merge.socio.climate, occtrain)
-      }
-      maxent.map <- predict(maxent.model, merge.socio.climate)
-      
+
+      maxent.model <<- maxent(merge.socio.climate, occtrain)
+      maxent.map <<- predict(maxent.model, merge.socio.climate)
+
+      progress$inc(0.5, detail = "Evaluating MaxEnt model")
+
       # Evaluating model
       # Generate background points
       bg <- randomPoints(merge.socio.climate, 1000)
-      e1 <- evaluate(maxent.model, p=occtest, a=bg, x=merge.socio.climate)
+      e1 <<- evaluate(maxent.model, p=occtest, a=bg, x=merge.socio.climate)
+
       # Identify thresholds
-      model.thresholds <- threshold(e1)
-
-     
-      # Render the map
-      output$map <- renderPlot({
-        plot(maxent.map, xlab="Longitude", ylab="Latitude")
-        points(occ, pch=20, cex=0.4)
-      })
-
-      # Plot variable significance
-      output$significance <- renderPlot({
-        plot(maxent.model)
-      })
-
-      # Plot the AUC curve
-      output$auc <- renderImage({
-        outfile <- paste0(maxent.model@path,"/plots/species_roc.png")
-        list(src = outfile,
-             contentType = 'image/png',
-             height = 390,
-             width = 520
-        )
-      })
-
-      # Create table of thresholds
-      output$threshold <- DT::renderDataTable(
-        DT::datatable(t(model.thresholds), options = list(searching = FALSE, paging = FALSE, info = FALSE, order = list(1, 'asc')))
-      )
-
-      updateTabsetPanel(session, "outputs", selected = "Model Map")
+      maxent.thresholds <- threshold(e1)
+    })
+    
+    observeEvent(input$models.map, {
+      if(input$models.map == "Maxent") {
+        leafletProxy("map") %>% clearImages() %>% clearControls() %>%
+        addRasterImage(layerId = "maxent", maxent.map, opacity=0.5,
+                       colors=brewer.pal(5, "YlOrRd")) %>%
+        addLegend(values=values(maxent.map), title="Likelihood", 
+                  colors=brewer.pal(5, "YlOrRd"), 
+                  labels=c("0.0-0.2","0.2-0.4","0.4-0.6","0.6-0.8","0.8-1.0"),
+                  position="bottomright")
+        
+        output$significance <- renderPlot({
+          plot(maxent.model)
+        })
+        output$auc <- renderPlot({
+          plot(e1, 'ROC')
+        })
+      }
+      else
+        leafletProxy("map") %>% clearImages()
     })
   }
 )
--- ui.R
+++ ui.R
@@ -1,85 +1,53 @@
-library(shiny)
-library(shinyBS)
+library(leaflet)
 
-shinyUI(fluidPage(
-  titlePanel("Vaccine-Preventable Outbreaks"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create maximum entropy maps with information from 
-               Council on Foreign Relations data on vaccine-preventable 
-               outbreaks."),
-      sliderInput("years", "Years of interest:",
-                  min = 2007, max = 2016, value = c(2007, 2016), tick=F, sep=""),
-     
-      checkboxGroupInput("categories",
-                         h4("Response Categories"),
-                         choices = list("Measles" = "Measles",
-                                        "Mumps" = "Mumps",
-                                        "Polio" = "Polio",
-                                        "Rubella" = "Rubella",
-                                        "Whooping Cough" = "Whooping Cough"),
-                         selected = c("Measles", "Mumps", "Polio", "Rubella", "Whooping Cough")),
-      
-      checkboxGroupInput("impact.scale",
-                         h4("Impact Scale"),
-                         choices = list("Epidemic" = "Epidemic",
-                                        "Secondary" = "Secondary",
-                                        "Cluster" = "Cluster",
-                                        "Isolated" = "Isolated"),
-                         selected=c("Epidemic","Secondary","Cluster","Isolated")),
-      radioButtons("fatalities",
-                         h4("Fatalities"),
-                         choices = list("Include only fatal outbreaks" = 1,
-                                        "Include only non-fatal outbreaks" = 0,
-                                        "Include all outbreaks regardless of fatalities" = -1),
-                   selected=-1),
-      
-      h4("Predictor Variables"),
-      bsCollapse(
-        bsCollapsePanel("Temperature",
-                        checkboxGroupInput("temperature", label=NULL,
-                                           choices = list("Annual Avg Temp (mat)" = 1,
-                                          "Avg Diurnal Range (mdr)" = 2,
-                                          "Isothermality (iso)" = 3,
-                                          "Temp Seasonality (tseas)" = 4,
-                                          "Max Temp of Warmest Month (tmax)" = 5,
-                                          "Min Temp of Coldest Month (tmin)" = 6,
-                                          "Temp Annual Range (tar)" = 7,
-                                          "Avg Temp of Wettest Quarter (twetq)" = 8,
-                                          "Avg Temp of Driest Quarter (tdryq)" = 9,
-                                          "Avg Temp of Warmest Quarter (twarmq)" = 10,
-                                          "Avg Temp of Coldest Quarter (tcoldq)" = 11),
-                           selected = 1:11)
+# Choices for drop-downs
+models <- c(
+  "MaxEnt" = "maxent",
+  "Boosted Regression Tree" = "brt"
+)
+
+
+navbarPage("Outbreak Modeling", id="nav",
+           
+           tabPanel("Interactive map",
+                    div(class="outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
                         ),
-        bsCollapsePanel("Precipitation",
-                        checkboxGroupInput("precipitation", label=NULL,
-                                           choices = list("Annual Precip (ap)" = 12,
-                                                          "Precip of Wettest Month (pwet)" = 13,
-                                                          "Precip of Driest Month (pdry)"= 14,
-                                                          "Precip Seasonality (pseas)" = 15,
-                                                          "Precip of Wettest Quarter (pwetq)" = 16,
-                                                          "Precip of Driest Quarter (pdryq)" = 17,
-                                                          "Precip of Warmest Quarter (pwarmq)" = 18,
-                                                          "Precip of Coldest Quarter (pcoldq)" = 19),
-                                           selected = 12:19)
+                        
+                        leafletOutput("map", width="100%", height="100%"),
+                        
+                        # Shiny versions prior to 0.11 should use class="modal" instead.
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      h2("Exploration"),
+                                      fileInput('data', 'Data file', accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                                      p("Modeling"),
+                                      actionButton("runModel", "Run Models"),
+                                      br(),br(),
+                                      radioButtons("models.map","View Models",
+                                                   c("Maxent","BRT"),
+                                                   selected = "Maxent")
                         ),
-        bsCollapsePanel("Socioeconomic",
-                        checkboxGroupInput("socio", label=NULL, 
-                                           choices = list("Population Density (glds00ag)" = 20),
-                                           selected=20)
+                        absolutePanel(id = "viz", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, right = "auto", left = 20, bottom = "auto",
+                                      width = 330, height = "auto",
+                                      h2("Plots"),
+                                      plotOutput("significance", height = 300),
+                                      plotOutput("auc", height = 300)
                         )
-      ),
-      actionButton("submitButton", "Submit")
-      ),
-    mainPanel(style="position:fixed; right:0;",
-      tabsetPanel(
-        tabPanel("Visualize", plotOutput("visualize"), helpText("Outbreaks matching the selected criteria are displayed and updated with changes in selections")),
-        tabPanel("Model Map", plotOutput("map"), helpText("Likelihood of outbreaks occuring using selected predictors is overlaid with known outbreaks")),
-        tabPanel("Predictor Significance", plotOutput("significance"), helpText("Each predictor's relative contribution in the maximum entropy model")),
-        tabPanel("ROC", imageOutput("auc"), helpText("Receiver operating characteristic (ROC) curve for the data")),
-        tabPanel("Thresholds", DT::dataTableOutput("threshold"), helpText("Thresholds to transform model predictions to presence, absence based on various criteria")),
-        id="outputs"
-      )
-    )
-  )
-))
+                    )
+           ),
+           tabPanel("Data explorer",
+                    fluidRow(
+                      DT::dataTableOutput("table")
+                    )
+                    ,
+                    conditionalPanel("false", icon("crosshair"))
+           )
+)
